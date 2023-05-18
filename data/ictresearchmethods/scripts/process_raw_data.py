b87b100abb2ec657824db62a3052af6db9022ec0
wensle <wensley.podrono@gmail.com>
2023-05-18 22:06:35 +0200

    Modify process_raw_data.py

--- data/ictresearchmethods/scripts/process_raw_data.py
+++ data/ictresearchmethods/scripts/process_raw_data.py
@@ -10,6 +10,7 @@ from dotsavvy.datastore.pinecone.types import METADATA
 # Constants
 _INPUT_DATA_DIR: Path = ICT_RESEARCH_METHODS_BASE_DIR / "raw_data"
 _OUTPUT_DATA_DIR: Path = ICT_RESEARCH_METHODS_BASE_DIR / "processed_data"
+_OUTPUT_JSONL_FILE: Path = _OUTPUT_DATA_DIR / "processed_data.jsonl"
 
 
 def create_processed_data_dir() -> None:
@@ -28,18 +29,19 @@ def process_raw_data_file(raw_data: dict[str]) -> dict[str, str | METADATA]:
     return {"text": text, "metadata": metadata}
 
 
-def run() -> None:
+def main() -> None:
     create_processed_data_dir()
-    output_data_file: Path = _OUTPUT_DATA_DIR / "processed_data.json"
     output_data = []
     for raw_data_file in _INPUT_DATA_DIR.glob("*.json"):
         with open(raw_data_file, "r", encoding="utf-8") as f:
             raw_data = json.load(f)
         processed_data = process_raw_data_file(raw_data)
         output_data.append(processed_data)
-    with open(output_data_file, "w", encoding="utf-8") as f:
-        json.dump(output_data, f, indent=4)
+    with open(_OUTPUT_JSONL_FILE, "w", encoding="utf-8") as f:
+        for data in output_data:
+            json.dump(data, f, ensure_ascii=False)
+            f.write("\n")
 
 
 if __name__ == "__main__":
-    run()
+    main()
