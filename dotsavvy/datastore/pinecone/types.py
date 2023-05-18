dc7ac9de8cab1f0e69292d07582b06c9becd67a4
wensle <wensley.podrono@gmail.com>
2023-05-18 23:18:24 +0200

    Define type for pinecone metadata

---
+++ dotsavvy/datastore/pinecone/types.py
@@ -0,0 +1,4 @@
+"""Type definitions for Pinecone."""
+METADATA = dict[str, str | int | float | bool | list[str]]
+"""Metadata associated with a vector in Pinecone is a key-value pair. This is
+represented as a dictionary."""
