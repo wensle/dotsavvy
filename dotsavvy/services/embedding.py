29fc1da24dcdab1fdefc58979c14d2738e852f25
wensle <wensley.podrono@gmail.com>
2023-05-20 21:50:28 +0200

    Add function for embedding multiple documents

--- dotsavvy/services/embedding.py
+++ dotsavvy/services/embedding.py
@@ -5,9 +5,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from dotsavvy.utils.env_variables import get_env_variable
 
 
-def embed_document(
-    text: str, embedding_model_name: str | None = None
-) -> List[List[float]]:
+def embed_document(text: str, embedding_model_name: str | None = None) -> List[float]:
     embedding_model_name = embedding_model_name or get_env_variable(
         "DOTSAVVY_EMBEDDING_MODEL_NAME"
     )
@@ -16,9 +14,22 @@ def embed_document(
     res: List[List[float]] = embed.embed_documents([text])
     if len(res) != 1:
         raise ValueError(
-            f"Expected exactly one embedding, got more. Text: {text}, embeddings: {res}"
+            f"Expected exactly one embedding, got {len(res)}. Text: {text}, "
+            f"embeddings: {res}"
         )
     return res[0]
+
+
+def embed_documents(
+    text: list[str], embedding_model_name: str | None = None
+) -> List[List[float]]:
+    embedding_model_name = embedding_model_name or get_env_variable(
+        "DOTSAVVY_EMBEDDING_MODEL_NAME"
+    )
+    open_api_key: str = get_env_variable("DOTSAVVY_OPENAI_API_KEY")
+    embed = OpenAIEmbeddings(model=embedding_model_name, openai_api_key=open_api_key)
+    res: List[List[float]] = embed.embed_documents(text)
+    return res
 
 
 def embed_query(text: str, embedding_model_name: str | None = None):
