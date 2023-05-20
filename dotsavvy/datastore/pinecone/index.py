df4533dda3ec1aedab600c036f716d485ff4bb72
wensle <wensley.podrono@gmail.com>
2023-05-20 17:22:29 +0200

    Refactor code and files related to pinecone index

--- dotsavvy/datastore/pinecone/index.py
+++ dotsavvy/datastore/pinecone/index.py
@@ -3,6 +3,15 @@ from pinecone import GRPCIndex, Index
 
 from dotsavvy.utils.env_variables import get_env_variable
 
+# Constants
+_DISTANCE_METRIC = "cosine"
+"""OpenAI recommends cosine similarity for text embeddings, see
+https://platform.openai.com/docs/guides/embeddings/which-distance-function-should-i-use
+"""
+_EMBEDDING_DIMENSION = 1536
+"""OpenAI's text-embedding-ada-002 model has an embedding dimension of 1536, see
+https://platform.openai.com/docs/guides/embeddings/second-generation-models"""
+
 # Global flag to indicate whether Pinecone has been initialized
 _pinecone_initialized = False
 
@@ -27,3 +36,19 @@ def get_REST_index() -> Index:
     init_pinecone()
     index_name: str = get_env_variable("DOTSAVVY_PINECONE_INDEX_NAME")
     return pinecone.Index(index_name)
+
+
+def create_index() -> None:
+    init_pinecone()
+    index_name: str = get_env_variable("DOTSAVVY_PINECONE_INDEX_NAME")
+    pinecone.create_index(
+        name=index_name,
+        dimension=_EMBEDDING_DIMENSION,
+        metric=_DISTANCE_METRIC,
+    )
+
+
+def delete_index() -> None:
+    init_pinecone()
+    index_name: str = get_env_variable("DOTSAVVY_PINECONE_INDEX_NAME")
+    pinecone.delete_index(name=index_name)
---
+++ dotsavvy/scripts/pinecone/delete_pineconde_index.py
@@ -0,0 +1,5 @@
+from dotsavvy.datastore.pinecone.index import delete_index
+
+
+if __name__ == "__main__":
+    delete_index()
