fd9368a309ccd0c12b185278a45cab799645dc79
wensle <wensley.podrono@gmail.com>
2023-05-20 16:46:19 +0200

    Change embed service for exactly one document

--- dotsavvy/services/embedding.py
+++ dotsavvy/services/embedding.py
@@ -7,8 +7,12 @@ from dotsavvy.utils.env_variables import get_env_variable
 _MODEL_NAME = "text-embedding-ada-002"
 
 
-def embed_documents(texts: List[str]) -> List[List[float]]:
+def embed_document(text: str) -> List[List[float]]:
     open_api_key: str = get_env_variable("DOTSAVVY_OPENAI_API_KEY")
     embed = OpenAIEmbeddings(model=_MODEL_NAME, openai_api_key=open_api_key)
-    res: List[List[float]] = embed.embed_documents(texts)
-    return res
+    res: List[List[float]] = embed.embed_documents([text])
+    if len(res) != 1:
+        raise ValueError(
+            f"Expected exactly one embedding, got more. Text: {text}, embeddings: {res}"
+        )
+    return res[0]
